6c1fee8a7d98b3270931c5a0659b2c296ffaac89
Osca Mwongera <oscamwongera@gmail.com>
2020-11-12 20:41:24 +0300

    initial commit, crud implementation in a golang web app

---
+++ go.mod
@@ -0,0 +1,5 @@
+module htmltemp
+
+go 1.14
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+// Note data model creating notes
+type Note struct {
+	Title       string
+	Description string
+	CreatedOn   time.Time
+}
+
+// EditNote data model for editing notes
+type EditNote struct {
+	Note
+	Id string
+}
+
+var noteStore = make(map[string]Note)
+var id int = 0
+var templates map[string]*template.Template
+
+func init() {
+	if templates == nil {
+		templates = make(map[string]*template.Template)
+	}
+	templates["index"] = template.Must(template.ParseFiles("templates/index.html", "templates/base.html"))
+	templates["add"] = template.Must(template.ParseFiles("templates/add.html", "templates/base.html"))
+	templates["edit"] = template.Must(template.ParseFiles("templates/edit.html", "templates/base.html"))
+}
+
+func renderTemplate(w http.ResponseWriter, name string, template string, viewModel interface{}) {
+	tmpl, ok := templates[name]
+
+	if !ok {
+		http.Error(w, "The template does not exist.", http.StatusInternalServerError)
+	}
+	err := tmpl.ExecuteTemplate(w, template, viewModel)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// Handler for "/notes/save" to save a new item into the datastore
+func saveNote(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r)
+	title := r.PostFormValue("title")
+	desc := r.PostFormValue("description")
+	note := Note{title, desc, time.Now()}
+	id++
+	k := strconv.Itoa(id)
+	noteStore[k] = note
+	http.Redirect(w, r, "/", 302)
+}
+
+func addNote(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "add", "base", nil)
+}
+
+func editNote(w http.ResponseWriter, r *http.Request) {
+	var viewModel EditNote
+	vars := mux.Vars(r)
+	k := vars["id"]
+	if note, ok := noteStore[k]; ok {
+		viewModel = EditNote{note, k}
+	} else {
+		http.Error(w, "Could not find the resource to edit.", http.StatusBadRequest)
+	}
+	renderTemplate(w, "edit", "base", viewModel)
+}
+
+func updateNote(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	k := vars["id"]
+	var noteToUpd Note
+	if note, ok := noteStore[k]; ok {
+		r.ParseForm()
+		noteToUpd.Title = r.PostFormValue("title")
+		noteToUpd.Description = r.PostFormValue("description")
+		noteToUpd.CreatedOn = note.CreatedOn
+		delete(noteStore, k)
+		noteStore[k] = noteToUpd
+	} else {
+		http.Error(w, "Could not find the resource to update.", http.StatusBadRequest)
+	}
+	http.Redirect(w, r, "/", 302)
+}
+
+func deleteNote(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	k := vars["id"]
+	if _, ok := noteStore[k]; ok {
+		delete(noteStore, k)
+	} else {
+		http.Error(w, "Could not find the resource to delete.", http.StatusBadRequest)
+	}
+	http.Redirect(w, r, "/", 302)
+}
+
+func getNotes(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index", "base", noteStore)
+}
+
+func main() {
+	r := mux.NewRouter().StrictSlash(false)
+	fs := http.FileServer(http.Dir("public"))
+	r.Handle("/public/", fs)
+	r.HandleFunc("/", getNotes)
+	r.HandleFunc("/notes/add", addNote)
+	r.HandleFunc("/notes/save", saveNote)
+	r.HandleFunc("/notes/edit/{id}", editNote)
+	r.HandleFunc("/notes/update/{id}", updateNote)
+	r.HandleFunc("/notes/delete/{id}", deleteNote)
+
+	server := &http.Server{
+		Addr:    ":5000",
+		Handler: r,
+	}
+
+	log.Println("Listening...")
+	server.ListenAndServe()
+}
